ce676a02d36bbdafbe1d3386ba530eb1b288f25f
Pawash97 <136896157+Pawash97@users.noreply.github.com>
2023-07-03 02:18:51 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -21,11 +21,7 @@ contract Assessment {
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
 
         // assert transaction completed successfully
@@ -35,20 +31,15 @@ contract Assessment {
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+            revert InsufficientBalance({ balance: balance, withdrawAmount: _withdrawAmount });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
         // assert the balance is correct
