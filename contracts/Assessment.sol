dfd73cb5618194af3baf7d1799d86b30659cd1ba
Pawash97 <136896157+Pawash97@users.noreply.github.com>
2023-07-07 03:08:46 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 //import "hardhat/console.sol";
@@ -9,19 +9,24 @@ contract Assessment {
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event neOwner(address indexed previousOwner, address indexed newOwner);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
+
+        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
+
+        // perform transaction
         balance += _amount;
 
         // assert transaction completed successfully
@@ -31,15 +36,17 @@ contract Assessment {
         emit Deposit(_amount);
     }
 
+    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
-            revert InsufficientBalance({ balance: balance, withdrawAmount: _withdrawAmount });
+            revert InsufficientBalance({balance: balance, withdrawAmount: _withdrawAmount});
         }
 
+        // withdraw the given amount
         balance -= _withdrawAmount;
 
         // assert the balance is correct
@@ -47,5 +54,20 @@ contract Assessment {
 
         // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function newOwner(address payable _anotherUser) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(_anotherUser != address(0), "Invalid address");
+        address payable _previousOwner = owner;
+        owner = _anotherUser;
+
+        // emit the event
+        emit newOwner(_previousOwner, _anotherUser);
+    }
+
+     function verifyPin(uint256 _pin) public view returns (bool) {
+        uint256 validPin = 1234;
+        return _pin == validPin;
     }
 }
